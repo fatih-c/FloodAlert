b8555ea8185b8b0612c863d3960591cca128d064
Fatih <fatih.crljenkovic@live.com>
2020-01-12 17:39:44 +0100

    fff

---
+++ FloodAlert/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FloodAlert
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            WebScraping file = new WebScraping();
+            string html = file.url;
+            int ind = html.IndexOf("id=\"nivo1\""); //<td>10.01.20 u 19:00</td>    <td align="right" id="nivo1">100,00 cm </td>
+            string level = html.Substring(ind+25, 9);
+            string vrijeme = html.Substring(ind - 25, 16);
+            txtBox1.Text = vrijeme + "    " + level;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FloodAlert/WebScraping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Net;
+
+namespace FloodAlert
+{
+    class WebScraping
+    {
+        private readonly WebClient client;
+        public string url;
+
+        public WebScraping()
+        {
+            client = new WebClient();
+            url = client.DownloadString("http://www.voda.ba/stanica?stanicabr=109");
+        }
+       
+    }
+}
+                  
